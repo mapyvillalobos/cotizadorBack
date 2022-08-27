62e65237c70ce30336d310050195523cdf344027
Mapy Villalobos <mapyvillalobos@Mapys-MacBook-Pro.local>
2022-08-27 16:13:09 -0500

    utils

---
+++ utils/utils.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+exports.clearRes = (data) => {
+  const { password, createdAt, updatedAt, __v, ...restData } = data;
+  return restData;
+};
+
+exports.createJWT = (user) => {
+  return jwt
+    .sign(
+      {
+        userId: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.SECRET,
+      { expiresIn: "24h" }
+    )
+    .split(".");
+};
